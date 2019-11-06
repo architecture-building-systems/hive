8fba0b933d65d557bbb25b02fb040442f7e921a4
Christoph Waibel <christophwaibel@gmx.de>
2019-11-06 16:55:34 +0100

    added sia2024 values for school, office, efh. sia380 results wrong?
    negative heating demand and transmission losses in SIA380.1 component when using this SIA2024 component

--- sia380/sia2024.py
+++ sia380/sia2024.py
@@ -5,7 +5,7 @@ Implements predefined default values from SIA 2024:2015 for SIA 380.1 calculatio
 """
 
 
-def default_values(use_case, type, area, month):
+def default_values(use_case, bldgtype, area, month, season):
     """
 
     :param use_case:
@@ -20,16 +20,16 @@ def default_values(use_case, type, area, month):
     theta_e Aussenlufttemperatur
     theta_i Raumlufttemperatur
     t       Länge der Berechnungsperiode [h]
-    A_op    Aussenwandfläche (opak) [m2]            !!!!!! A_{th} in sia2024 - thermische Gebäudehüllfläche 
+    A_th    Thermische Gebäudehüllfläche [m2] 
     A_w     Fensterfläche [m2]                      !!!!!! f_g in sia2024 - Glasanteil in [%]
     U_op    Wärmedurchgangskoeffizient Aussenwand [W/m2K]
     U_w     Wärmedurchgangskoeffizient Fenster [W/m2K]
-    Vdot_e  Aussenluft-Volumenstrom [m3/h]
-    Vdot_inf Aussenluft-Volumenstrom durch Infiltration [m3/h]
+    q_ve  Aussenluft-Volumenstrom [m3/m2h]
+    q_vinf Aussenluft-Volumenstrom durch Infiltration [m3/m2h]
     eta_rec Nutzungsgrad der Wärmerückgewinnung [-]
-    phi_P   Wärmeabgabe Personen [W]
-    phi_L   Wärmeabgabe Beleuchtung [W]
-    phi_A   Wärmeabgabe Geräte [W]
+    phi_P   Wärmeabgabe Personen [W/m2]
+    phi_L   Wärmeabgabe Beleuchtung [W/m2]
+    phi_A   Wärmeabgabe Geräte [W/m2]
     t_P     Vollaststunden Personen [h]
     t_L     Vollaststunden Beleuchtung [h]
     t_A     Vollaststunden Geräte [h]
@@ -38,6 +38,7 @@ def default_values(use_case, type, area, month):
     I       description": "Solare Strahlung [Wh/m2]
     """
 
+    summerstart, summerend = 3, 10
     dayspermonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
     # Data according to testcase in sia380.py
@@ -56,7 +57,7 @@ def default_values(use_case, type, area, month):
         - 2.1   ...:        Hotelroom                                               HNF1
         - 2.2   ...:        Hotel lobby                                             HNF1
         - ...   ...:
-        - 3.2   office:     small office space (Einzel,- Gruppenbüro)               HNF2
+        - 3.1   office:     small office space (Einzel,- Gruppenbüro)               HNF2
         - ...   ...:
         - 4.1   school:     school room (Schulzimmer)                               HNF5
         - ...   ...:
@@ -66,13 +67,15 @@ def default_values(use_case, type, area, month):
     # following data implements standard values of SIA 2024:2015 - table 2, p.10 (Standardwert)
     use_case_data = {
         "mfh": {'tau': 182.0,
-                'theta_i': 26.0,
-                'A_op': 26.0,
+                'theta_i_summer': 26.0,
+                'theta_i_winter': 21.0,
+                'A_NGF': 20.0,
+                'A_th': 26.0,
                 'A_w': 26.0 * 0.3, # 30%... west window?! sia2024
                 'U_op': 0.2,
                 'U_w': 1.2,
-                'Vdot_e': 1.0,
-                'Vdot_inf': 0.15,
+                'q_ve': 1.0,
+                'q_vinf': 0.15,
                 'eta_rec': 0.75,
                 'phi_P': 2.3,
                 'phi_L': 2.7,
@@ -82,14 +85,16 @@ def default_values(use_case, type, area, month):
                 't_A': [1780.0] * 12,
                 'g': 0.5},
         "efh": {'tau': 164.0,
-                'theta_i': 26.0,
-                'A_op': 20.0,
-                'A_w': 38.0,
+                'theta_i_summer': 26.0,
+                'theta_i_winter': 21.0,
+                'A_NGF': 20.0,
+                'A_th': 38.0,
+                'A_w': 38.0 * 0.3,
                 'U_op': 0.2,
                 'U_w': 1.2,
-                'Vdot_e': 0.6,
-                'Vdot_inf': 0.15,
-                'eta_rec': 0.75,
+                'q_ve': 0.6,
+                'q_vinf': 0.15,
+                'eta_rec': 0.7,
                 'phi_P': 1.4,
                 'phi_L': 2.7,
                 'phi_A': 8.0,
@@ -98,49 +103,55 @@ def default_values(use_case, type, area, month):
                 't_A': [1780.0] * 12,
                 'g': 0.5},
         "office": {'tau': 117.0,
-                   'theta_i': 26.0,
-                   'A_op': 36.0,
-                   'A_w': 42.0,
+                   'theta_i_summer': 26.0,
+                   'theta_i_winter': 21.0,
+                   'A_NGF': 36.0,
+                   'A_th': 42.0,
+                   'A_w': 42.0 * 0.5,
                    'U_op': 0.2,
                    'U_w': 1.2,
-                   'Vdot_e': 2.6,
-                   'Vdot_inf': 0.15,
+                   'q_ve': 2.6,
+                   'q_vinf': 0.15,
                    'eta_rec': 0.75,
                    'phi_P': 5.0,
                    'phi_L': 15.9,
                    'phi_A': 7.0,
-                   't_P': [4090.0] * 12,
-                   't_L': [1450.0] * 12,
-                   't_A': [1780.0] * 12,
+                   't_P': [1500.0] * 12,
+                   't_L': [1210.0] * 12,
+                   't_A': [1930.0] * 12,
                    'g': 0.5},
         "school": {'tau': 72.0,
-                   'theta_i': 26.0,
-                   'A_op': 70.0,
-                   'A_w': 95.0,
+                   'theta_i_summer': 26.0,
+                   'theta_i_winter': 21.0,
+                   'A_NGF': 70.0,
+                   'A_th': 95.0,
+                   'A_w': 95.0 * 0.5,
                    'U_op': 0.2,
                    'U_w': 1.2,
-                   'Vdot_e': 8.3,
-                   'Vdot_inf': 0.15,
-                   'eta_rec': 0.75,
+                   'q_ve': 8.3,
+                   'q_vinf': 0.15,
+                   'eta_rec': 0.7,
                    'phi_P': 23.3,
                    'phi_L': 14.0,
                    'phi_A': 4.0,
-                   't_P': [4090.0] * 12,
-                   't_L': [1450.0] * 12,
-                   't_A': [1780.0] * 12,
+                   't_P': [1390.0] * 12,
+                   't_L': [1180.0] * 12,
+                   't_A': [1770.0] * 12,
                    'g': 0.5}
     }
 
     # following data implements ideal values of SIA 2024:2015 - table 2, p.10 (Zielwert)
     use_case_data_ideal = {
-        "mfh": {'tau': 258.0, #
-                'theta_i': 26.0,
-                'A_op': 26.0,
+        "mfh": {'tau': 258.0,
+                'theta_i_summer': 26.0,
+                'theta_i_winter': 21.0,
+                'A_NGF': 20.0,
+                'A_th': 26.0,
                 'A_w': 26.0 * 0.3,
                 'U_op': 0.1,
                 'U_w': 0.9,
-                'Vdot_e': 1.0,
-                'Vdot_inf': 0.15,
+                'q_ve': 1.0,
+                'q_vinf': 0.15,
                 'eta_rec': 0.8,
                 'phi_P': 2.3,
                 'phi_L': 1.7,
@@ -149,66 +160,74 @@ def default_values(use_case, type, area, month):
                 't_L': [1110.0] * 12,
                 't_A': [1780.0] * 12,
                 'g': 0.5},
-        "efh": {'tau': 164.0,
-                'theta_i': 26.0,
-                'A_op': 20.0,
-                'A_w': 38.0,
-                'U_op': 0.2,
-                'U_w': 1.2,
-                'Vdot_e': 0.6,
-                'Vdot_inf': 0.15,
-                'eta_rec': 0.75,
+        "efh": {'tau': 238.0,
+                'theta_i_summer': 26.0,
+                'theta_i_winter': 21.0,
+                'A_NGF': 20.0,
+                'A_th': 38.0,
+                'A_w': 38.0 * 0.3,
+                'U_op': 0.1,
+                'U_w': 0.9,
+                'q_ve': 0.6,
+                'q_vinf': 0.15,
+                'eta_rec': 0.8,
                 'phi_P': 1.4,
-                'phi_L': 2.7,
-                'phi_A': 8.0,
+                'phi_L': 1.7,
+                'phi_A': 4.0,
                 't_P': [4090.0] * 12,
-                't_L': [1450.0] * 12,
+                't_L': [1110.0] * 12,
                 't_A': [1780.0] * 12,
                 'g': 0.5},
-        "office": {'tau': 117.0,
-                   'theta_i': 26.0,
-                   'A_op': 36.0,
-                   'A_w': 42.0,
-                   'U_op': 0.2,
-                   'U_w': 1.2,
-                   'Vdot_e': 2.6,
-                   'Vdot_inf': 0.15,
-                   'eta_rec': 0.75,
+        "office": {'tau': 163.0,
+                   'theta_i_summer': 26.0,
+                   'theta_i_winter': 21.0,
+                   'A_NGF': 36.0,
+                   'A_th': 42.0,
+                   'A_w': 42.0 * 0.5,
+                   'U_op': 0.1,
+                   'U_w': 0.9,
+                   'q_ve': 2.6,
+                   'q_vinf': 0.15,
+                   'eta_rec': 0.85,
                    'phi_P': 5.0,
-                   'phi_L': 15.9,
-                   'phi_A': 7.0,
-                   't_P': [4090.0] * 12,
-                   't_L': [1450.0] * 12,
-                   't_A': [1780.0] * 12,
+                   'phi_L': 11.6,
+                   'phi_A': 3.0,
+                   't_P': [1500.0] * 12,
+                   't_L': [470.0] * 12,
+                   't_A': [1930.0] * 12,
                    'g': 0.5},
         "school": {'tau': 72.0,
-                   'theta_i': 26.0,
-                   'A_op': 70.0,
-                   'A_w': 95.0,
-                   'U_op': 0.2,
-                   'U_w': 1.2,
-                   'Vdot_e': 8.3,
-                   'Vdot_inf': 0.15,
-                   'eta_rec': 0.75,
+                   'theta_i_summer': 26.0,
+                   'theta_i_winter': 21.0,
+                   'A_NGF': 70.0,
+                   'A_th': 95.0,
+                   'A_w': 95.0 * 0.5,
+                   'U_op': 0.1,
+                   'U_w': 0.9,
+                   'q_ve': 8.3,
+                   'q_vinf': 0.15,
+                   'eta_rec': 0.8,
                    'phi_P': 23.3,
-                   'phi_L': 14.0,
-                   'phi_A': 4.0,
-                   't_P': [4090.0] * 12,
-                   't_L': [1450.0] * 12,
-                   't_A': [1780.0] * 12,
+                   'phi_L': 10.3,
+                   'phi_A': 2.0,
+                   't_P': [1390.0] * 12,
+                   't_L': [560.0] * 12,
+                   't_A': [1770.0] * 12,
                    'g': 0.5}
     }
 
     # following data implements values for existing buildings of SIA 2024:2015 - table 2, p.10 (Bestand)
     use_case_data_existing = {
         "mfh": {'tau': 68.0,
-                'theta_i': 26.0,
-                'A_op': 26.0,
+                'theta_i_summer': 26.0,
+                'theta_i_winter': 21.0,
+                'A_NGF': 20.0,
+                'A_th': 26.0,
                 'A_w': 26.0 * 0.3,
                 'U_op': 0.8,
                 'U_w': 1.5,
-                'Vdot_e': 1.0,
-                'Vdot_inf': 0.3,
+                'q_ve': 1.0,
+                'q_vinf': 0.3,
                 'eta_rec': 0.0,
                 'phi_P': 2.3,
                 'phi_L': 2.7,
@@ -217,64 +236,70 @@ def default_values(use_case, type, area, month):
                 't_L': [1450.0] * 12,
                 't_A': [1780.0] * 12,
                 'g': 0.65},
-        "efh": {'tau': 164.0,
-                'theta_i': 26.0,
-                'A_op': 20.0,
-                'A_w': 38.0,
-                'U_op': 0.2,
-                'U_w': 1.2,
-                'Vdot_e': 0.6,
-                'Vdot_inf': 0.15,
-                'eta_rec': 0.75,
+        "efh": {'tau': 56.0,
+                'theta_i_summer': 26.0,
+                'theta_i_winter': 21.0,
+                'A_NGF': 20.0,
+                'A_th': 38.0,
+                'A_w': 38.0 * 0.3,
+                'U_op': 0.8,
+                'U_w': 1.5,
+                'q_ve': 0.6,
+                'q_vinf': 0.3,
+                'eta_rec': 0.0,
                 'phi_P': 1.4,
                 'phi_L': 2.7,
-                'phi_A': 8.0,
+                'phi_A': 10.0,
                 't_P': [4090.0] * 12,
                 't_L': [1450.0] * 12,
                 't_A': [1780.0] * 12,
-                'g': 0.5},
-        "office": {'tau': 117.0,
-                   'theta_i': 26.0,
-                   'A_op': 36.0,
-                   'A_w': 42.0,
-                   'U_op': 0.2,
-                   'U_w': 1.2,
-                   'Vdot_e': 2.6,
-                   'Vdot_inf': 0.15,
-                   'eta_rec': 0.75,
+                'g': 0.65},
+        "office": {'tau': 61.0,
+                   'theta_i_summer': 26.0,
+                   'theta_i_winter': 21.0,
+                   'A_NGF': 36.0,
+                   'A_th': 42.0,
+                   'A_w': 42.0 * 0.5,
+                   'U_op': 0.8,
+                   'U_w': 1.5,
+                   'q_ve': 2.6,
+                   'q_vinf': 0.3,
+                   'eta_rec': 0.5,
                    'phi_P': 5.0,
                    'phi_L': 15.9,
-                   'phi_A': 7.0,
-                   't_P': [4090.0] * 12,
-                   't_L': [1450.0] * 12,
-                   't_A': [1780.0] * 12,
-                   'g': 0.5},
+                   'phi_A': 15.0,
+                   't_P': [1500.0] * 12,
+                   't_L': [1210.0] * 12,
+                   't_A': [1930.0] * 12,
+                   'g': 0.65},
         "school": {'tau': 72.0,
-                   'theta_i': 26.0,
-                   'A_op': 70.0,
-                   'A_w': 95.0,
-                   'U_op': 0.2,
-                   'U_w': 1.2,
-                   'Vdot_e': 8.3,
-                   'Vdot_inf': 0.15,
-                   'eta_rec': 0.75,
+                   'theta_i_summer': 26.0,
+                   'theta_i_winter': 21.0,
+                   'A_NGF': 70.0,
+                   'A_th': 95.0,
+                   'A_w': 95.0 * 0.5,
+                   'U_op': 0.8,
+                   'U_w': 1.5,
+                   'q_ve': 8.3,
+                   'q_vinf': 0.3,
+                   'eta_rec': 0.0,
                    'phi_P': 23.3,
                    'phi_L': 14.0,
-                   'phi_A': 4.0,
-                   't_P': [4090.0] * 12,
-                   't_L': [1450.0] * 12,
-                   't_A': [1780.0] * 12,
-                   'g': 0.5}
+                   'phi_A': 6.0,
+                   't_P': [1390.0] * 12,
+                   't_L': [1180.0] * 12,
+                   't_A': [1770.0] * 12,
+                   'g': 0.65}
     }
 
-    if type == "standard":
+    if bldgtype == "standard":
         tmp = use_case_data[use_case]
-    elif type == "ideal":
+    elif bldgtype == "ideal":
         tmp = use_case_data_ideal[use_case]
     else:
         tmp = use_case_data_existing[use_case]
 
-    I = [37778.0, 46944.0, 57222.0, 53611.0, 56389.0, 53889.0, 56944.0, 56389.0, 50278.0, 45000.0, 32778.0, 29444.0] # sia380.py; where is that from?
+    # I = [37778.0, 46944.0, 57222.0, 53611.0, 56389.0, 53889.0, 56944.0, 56389.0, 50278.0, 45000.0, 32778.0, 29444.0] # sia380.py; where is that from?
     f_sh = 0.9  # sia2024, p.12, 1.3.1.9 Reduktion solare Wärmeeinträge
 
     # transforming yearly sia2024 data to monthly
@@ -283,13 +308,30 @@ def default_values(use_case, type, area, month):
         tmp['t_L'][i] *= dayspermonth[i] / 365.0
         tmp['t_A'][i] *= dayspermonth[i] / 365.0
 
-    return tmp['tau'], theta_e[month - 1], tmp['theta_i'], t[month - 1], tmp['A_op'], tmp['A_w'], tmp['U_op'], \
-           tmp['U_w'], tmp['Vdot_e'] * area, tmp['Vdot_inf'] * area, tmp['eta_rec'], tmp['phi_P'] * area, \
-           tmp['phi_L'] * area, tmp['phi_A'] * area, tmp['t_P'][month - 1], tmp['t_L'][month - 1], \
-           tmp['t_A'][month - 1], tmp['g'], f_sh, I[month - 1]
+    if (season == None):
+        if month >= summerstart and month <= summerend:
+            theta_i = tmp['theta_i_summer']
+        else:
+            theta_i = tmp['theta_i_winter']
+    else:
+        if season =="summer":
+            theta_i = tmp['theta_i_summer']
+        else:
+            theta_i = tmp['theta_i_winter']
+
+
+    if (area == None):
+        area = tmp['A_NGF']
+
+    return tmp['tau'], theta_e[month - 1], theta_i, t[month - 1], \
+           tmp['A_th'] - tmp['A_w'], tmp['A_w'], tmp['U_op'], tmp['U_w'], \
+           tmp['q_ve'] * area, tmp['q_vinf'] * area, tmp['eta_rec'], \
+           tmp['phi_P'] * area, tmp['phi_L'] * area, tmp['phi_A'] * area, \
+           tmp['t_P'][month - 1], tmp['t_L'][month - 1], tmp['t_A'][month - 1], \
+           tmp['g'], f_sh
 
 
 if __name__ == '__main__':
-    values = default_values("mfh", "standard", 500.0, 1)
+    values = default_values("mfh", "standard", None, 1, "winter")
     for i in range(len(values)):
         print(values[i])
