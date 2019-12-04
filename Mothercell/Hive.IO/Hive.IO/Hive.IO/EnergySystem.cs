107c10cd2ab9b711464d0550aba748f89a6876fb
Christoph Waibel <christophwaibel@gmx.de>
2019-12-04 16:13:27 +0100

    Hive.IO, components to handle inputs and outputs from Rhino to and from the mothercell

---
+++ Mothercell/Hive.IO/Hive.IO/Hive.IO/EnergySystem.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hive.IO
+{
+    /// <summary>
+    /// Namespace for Energy Systems geometry and properties
+    /// </summary>
+    namespace EnergySystem
+    {
+        public abstract class SurfaceSystem
+        {
+            public double Area { get; private set; }
+            public double RefEfficiency { get; private set; }
+            public double[] Irradiance { get; private set; }
+            public double[] AirTemperature { get; private set; }
+            public int Horizon { get; private set; }
+
+            protected SurfaceSystem(double area, double refEfficiency, double[] irradiance, double[] airTemperature)
+            {
+                Area = area;
+                RefEfficiency = refEfficiency;
+                Irradiance = irradiance;
+                AirTemperature = airTemperature;
+                Horizon = GetHorizon(Irradiance, AirTemperature);
+            }
+
+            protected static int GetHorizon(double[] array1, double[] array2) { return (array1.Length < array2.Length) ? array1.Length : array2.Length; }
+        }
+
+
+        /// <summary>
+        /// Photovoltaic
+        /// </summary>
+        public class PV : SurfaceSystem
+        {
+            public PV(double area, double refEfficiency, double[] irradiance, double[] airTemperature) 
+                : base(area, refEfficiency, irradiance, airTemperature){ }
+        }
+
+
+        /// <summary>
+        /// Solar Thermal
+        /// </summary>
+        public class ST : SurfaceSystem
+        {
+            public ST(double area, double refEfficiency, double[] irradiance, double[] airTemperature)
+                : base(area, refEfficiency, irradiance, airTemperature) { }
+        }
+
+
+        /// <summary>
+        /// Hybrid Solar Photovolatic Thermal
+        /// </summary>
+        public class PVT : SurfaceSystem
+        {
+            public PVT(double area, double refEfficiency, double[] irradiance, double[] airTemperature)
+                : base(area, refEfficiency, irradiance, airTemperature) { }
+        }
+
+
+        /// <summary>
+        /// Horizontal Ground Solar Collector
+        /// </summary>
+        public class GroundCollector : SurfaceSystem
+        {
+            public GroundCollector(double area, double refEfficiency, double[] irradiance, double[] airTemperature)
+                : base(area, refEfficiency, irradiance, airTemperature) { }
+        }
+    }
+}
---
+++ Mothercell/Hive.IO/Hive.IO/Hive.IO/HiveIOInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace Hive.IO
+{
+    public class HiveIOInfo : GH_AssemblyInfo
+  {
+    public override string Name
+    {
+        get
+        {
+            return "HiveIO";
+        }
+    }
+    public override Bitmap Icon
+    {
+        get
+        {
+            //Return a 24x24 pixel bitmap to represent this GHA library.
+            return null;
+        }
+    }
+    public override string Description
+    {
+        get
+        {
+            //Return a short string describing the purpose of this GHA library.
+            return "";
+        }
+    }
+    public override Guid Id
+    {
+        get
+        {
+            return new Guid("1da9c6d0-f813-4e78-8c56-fc2672430613");
+        }
+    }
+
+    public override string AuthorName
+    {
+        get
+        {
+            //Return a string identifying you or your company.
+            return "ETH Zurich";
+        }
+    }
+    public override string AuthorContact
+    {
+        get
+        {
+            //Return a string representing your preferred contact details.
+            return "";
+        }
+    }
+}
+}
