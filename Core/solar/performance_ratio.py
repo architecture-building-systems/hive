dd402ce02f2e04acc0b6249e6c580ccad31b457c
Christoph Waibel <christophwaibel@gmx.de>
2020-01-24 15:31:45 +0100

    formatting of json

--- Core/solar/performance_ratio.py
+++ Core/solar/performance_ratio.py
@@ -3,6 +3,7 @@
 The data is (should be) based on the lecture Energie und Klimasysteme  II,
 Erneuerbare Energieerzeugung am Gebäude, FS 2019, Folie 30,
 """
+from __future__ import print_function
 
 
 def get_performance_ratio(performance_scenario):
