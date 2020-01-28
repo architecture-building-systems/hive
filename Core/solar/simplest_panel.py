950adf34d2028f05000cb32c1b1b790137c42fda
Christoph Waibel <christophwaibel@gmx.de>
2020-01-28 12:00:31 +0100

    simple pv was already there. updated, merged, and fixed some other stuff

--- Core/solar/simplest_panel.py
+++
@@ -1,9 +0,0 @@
-"""
-Calculates annual solar electricity yield using simplest equation from Energy and Climate Systems course FS2019:
-E_PV = G x F_F x A x eta_PV x PR
-"""
-
-
-def main(G, F_F, A, eta_PV, PR):
-    E_PV = G * F_F * A * eta_PV * PR
-    return E_PV
