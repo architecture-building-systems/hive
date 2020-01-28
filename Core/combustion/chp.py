9d46bea7eefbde7257b00820e2658bf25523454f
Christoph Waibel <christophwaibel@gmx.de>
2020-01-28 15:04:39 +0100

    starting combustion techs

---
+++ Core/combustion/chp.py
@@ -0,0 +1,9 @@
+"""
+should do same as boiler, but it has both electricity and heating output
+So what are inputs / outputs then?
+Maybe component that could work with both:
+either input electricity, and outputs heating laods
+or input heating load, and it tells how much electrity we also get
+
+output for sure: cost and carbon
+"""
