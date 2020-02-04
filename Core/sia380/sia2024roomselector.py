d757c51999ba50944e2cb170a9eeca7f31ccdf3d
Christoph Waibel <christophwaibel@gmx.de>
2020-02-04 13:53:07 +0100

    sia2024 now loads data from database csv files

--- Core/sia380/sia2024roomselector.py
+++
@@ -1,9 +0,0 @@
-"""
-Returns SIA 2024 room data given a specific room type
-"""
-from __future__ import print_function
-
-
-def show_room(room):
-    return "\n".join("{key}: {value}".format(key=key, value=value)
-                     for key, value in room.items())
