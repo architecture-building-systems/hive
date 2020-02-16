7f5e451bc8b2bd4def47f1aeb943478d8d5dccee
Christoph Waibel <christophwaibel@gmx.de>
2020-02-16 18:09:55 +0100

    sketch windows form for Hive.Input Building component

---
+++ Mothercell/Hive.IO/Hive.IO/FormBuilding.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hive.IO
+{
+    public partial class FormBuilding : Form
+    {
+        public FormBuilding()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
