1c5d2b7b822abef957104d8d370f598a4c8f6688
Christoph Waibel <christophwaibel@gmx.de>
2020-02-18 15:27:01 +0100

    working on various Building-related classes

---
+++ Mothercell/Hive.IO/Hive.IO/Environment.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using rg = Rhino.Geometry;
+
+namespace Hive.IO
+{
+    /// <summary>
+    /// Environment class, defining the environment the building is embedded in. 
+    /// Contains climate data, location, adjacent obstacles and trees, etc.
+    /// Grid access and renewable potentials as well?
+    /// </summary>
+    public class Environment
+    {
+        public Climate ClimateData;
+        public Location LocationData;
+
+        public Environment(string epwFilePath)
+        {
+
+        }
+
+        /// <summary>
+        /// parse data from an epw into LocationData and ClimateData
+        /// </summary>
+        private void ReadEPW()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void SetLocation()
+        {
+
+        }
+
+        private void SetClimate()
+        {
+
+        }
+
+
+        /// <summary>
+        /// Location properties
+        /// </summary>
+        public struct Location
+        {
+            public int TimeZone;
+            public double Altitude;
+            public double Longitude;
+            public double Latitude;
+        }
+
+        /// <summary>
+        /// Climate properties
+        /// </summary>
+        public struct Climate
+        {
+            public double[] GHI;
+            public double[] DNI;
+            public double[] DHI;
+            public double[] DryBulbTemperature;
+            public double[] RelativeHumidity;
+            public double[] WindSpeed;
+            public double[] WindDirection;
+            public double[] Precipitation;
+            public double[] Snowfall;
+        }
+
+    }
+}
---
+++ Mothercell/Hive.IO/Hive.IO/ShadingDevice.cs
@@ -0,0 +1,83 @@
+﻿using rg = Rhino.Geometry;
+
+namespace Hive.IO
+{
+    /// <summary>
+    /// Static shading object, like louvers. Adjacent buildings are part of 'Environment.cs'
+    /// Can be mesh or brep.
+    /// </summary>
+    public class ShadingDevice
+    {
+        /// <summary>
+        /// indicating whether this shading device is dynamic, meaning it can move, like a louver. if false, it is static
+        /// </summary>
+        public bool IsDynamic { get; private set; }
+
+        /// <summary>
+        /// Indicating whether this shading device is internal, i.e. inside the zone.
+        /// </summary>
+        public bool IsInternal { get; private set; }
+
+        /// <summary>
+        /// Absorptivity
+        /// </summary>
+        public double Absorbtivity { get; private set; }
+        /// <summary>
+        /// Reflectivity
+        /// </summary>
+        public double Reflectivity { get; private set; }
+        /// <summary>
+        /// Transmissivity
+        /// </summary>
+        public double Transmissivity { get; private set; }
+        /// <summary>
+        /// Shading geometry as mesh. 
+        /// Is an array, since it may contain different states, but only when (this.IsDynamic == true), otherwise this.ShadingGeometry.Length = 1
+        /// </summary>
+        public rg.Mesh [] ShadingGeometry { get; private set; }
+
+        /// <summary>
+        /// Time horizon for the schedule
+        /// </summary>
+        public int Horizon { get; private set; }
+
+        /// <summary>
+        /// Time-resolved geometry schedule of the shading device. Only necessary if (this.IsDynamic == true)
+        /// It refers to the indices in this.ShadingGeometry.
+        /// E.g. this.GeometrySchedule[0] = 1 means that at timestep 0, geometry state 1 is active
+        /// </summary>
+        public int [] GeometrySchedule { get; private set; }
+
+        /// <summary>
+        /// Time-resolved transmissivity schedule [0,1].
+        /// When this.TransmissivitySchedule[timestep] = 1, then the transmissivity of the shading device at that timestep equals this.Transmissivity.
+        /// Only necessary if (this.IsDynamic == true)
+        /// </summary>
+        public double [] TransmissivitySchedule { get; private set; }
+        public double [] AbsorbtivitySchedule { get; private set; }
+        public double [] ReflectivitySchedule { get; private set; }
+
+        /// <summary>
+        /// Index that serves as zone identifier. E.g. ZoneIdentifier = 0 means that this ShadingDevice belongs to Zone 0
+        /// Shading device without zone can't exist. Otherwise, it would belong to Environment.cs as obstacle object
+        /// </summary>
+        public int ZoneIdentifier { get; private set; }
+
+
+
+        public ShadingDevice(rg.Mesh [] shadingGeometry, int zoneIdentifier)
+        {
+            ShadingGeometry = shadingGeometry;
+            ZoneIdentifier = zoneIdentifier;
+
+            // initialize with standard properties
+            Reflectivity = 0.1;
+            Transmissivity = 0.1;
+            Absorbtivity = 0.1;
+            IsDynamic = false;
+
+        }
+
+
+    }
+}
