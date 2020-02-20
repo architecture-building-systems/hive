ee620552ee9da44844227110b7077252df584535
Christoph Waibel <christophwaibel@gmx.de>
2020-02-20 14:58:45 +0100

    working on Hive Building Class

---
+++ Mothercell/Hive.IO/Hive.IO/BuildingConstruction.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Hive.IO
+{
+    namespace BuildingConstruction
+    {
+        /// <summary>
+        /// Opaque construction element
+        /// </summary>
+        public struct Opaque
+        {
+            /// <summary>
+            /// Name of construction element, e.g. 'concrete30mm'
+            /// </summary>
+            public string Name;
+            /// <summary>
+            /// Material name, e.g. 'concrete'
+            /// </summary>
+            public string Material;
+            /// <summary>
+            /// [mm]
+            /// </summary>
+            public double Thickness;
+        }
+
+        public struct Transparent
+        {
+
+        }
+    }
+}
---
+++ Mothercell/Hive.IO/Hive.IO/BuildingMaterial.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hive.IO
+{
+    namespace BuildingMaterial
+    {
+        public struct Opaque
+        {
+            /// <summary>
+            /// Name
+            /// </summary>
+            public string Name;
+            /// <summary>
+            /// lambda, [W/mK]
+            /// </summary>
+            public double Conductivity;
+            /// <summary>
+            /// rho, [kg/m^3]
+            /// </summary>
+            public double Density;
+            /// <summary>
+            /// c, [J/kgK]
+            /// </summary>
+            public double SpecificHeatCapacity;
+            /// <summary>
+            /// [-]
+            /// </summary>
+            public double ThermalAbsorptance;
+            /// <summary>
+            /// [-]
+            /// </summary>
+            public double SolarAbsorptance;
+            /// <summary>
+            /// [-]
+            /// </summary>
+            public double VisibleAbsorptance;
+        }
+
+        public struct Glass
+        {
+
+        }
+
+        public struct Gas
+        {
+
+        }
+
+    }
+}
