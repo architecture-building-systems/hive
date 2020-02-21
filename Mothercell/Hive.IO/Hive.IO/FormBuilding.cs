9fd863eb45f65e45ac6e9bb71fbdadac705d357d
Christoph Waibel <christophwaibel@gmx.de>
2020-02-21 16:34:59 +0100

    trying to get rhino viewport interaction from grasshopper windows form. looking at rhinocommon examples now

--- Mothercell/Hive.IO/Hive.IO/FormBuilding.cs
+++ Mothercell/Hive.IO/Hive.IO/FormBuilding.cs
@@ -7,14 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ri = Rhino.Input.Custom;
 
 namespace Hive.IO
 {
     public partial class FormBuilding : Form
     {
+        public Rhino.RhinoDoc RhDoc;
         public FormBuilding()
         {
             InitializeComponent();
+        }
+
+        public void SetRhinoDoc(Rhino.RhinoDoc doc)
+        {
+            this.RhDoc = doc;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -50,6 +57,11 @@ namespace Hive.IO
         private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
         {
 
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            //ri.GetLine();
         }
     }
 }
