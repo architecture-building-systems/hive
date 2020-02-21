ac1ab20870ab78eeb3955711223bb1d2cf814f5a
Christoph Waibel <christophwaibel@gmx.de>
2020-02-21 14:03:25 +0100

    WIP building.cs and zone.cs

--- Mothercell/Hive.IO/Hive.IO/Building.cs
+++ Mothercell/Hive.IO/Hive.IO/Building.cs
@@ -11,17 +11,67 @@ namespace Hive.IO
     public class Building
     {
         /// <summary>
+        /// Building Zones
+        /// </summary>
+        public Zone[] Zones { get; private set; }
+
+        /// <summary>
         /// Indicating adjacencies between zones. 
         /// First index is a sorted list of indices 0,1,2,...
         /// Second index is an array of indices that a zone is connected to.
         /// E.g. Adjacencies[0] = {1,2,3} means that zone 0 is connected to zones 1,2,3
         /// </summary>
         public int[][] Adjacencies { get; private set; }
-        
-        public Shading [] ShadingDevices { get; private set; }
+
+        /// <summary>
+        /// Describes the building type, e.g. residential, office, school, etc.
+        /// </summary>
+        public BuildingType Type { get; private set; }
+
+        public Building(Zone[] zones, BuildingType type)
+        {
+            this.Zones = zones;
+            this.Type = type;
+
+            // Hive 0.2
+            //this.Adjacencies = SolveAdjacencies(this.Zones);
+        }
 
 
+        //// Hive 0.2
+        ///// <summary>
+        ///// Solving zone adjacencies
+        ///// </summary>
+        ///// <param name="zones"></param>
+        ///// <param name="adjacencies"></param>
+        //private int [][] SolveAdjacencies(Zone[] zones)
+        //{
+
+        //    return new int[zones.Length][];
+
+        //    // where are congruent surfaces solved? here?
+        //    // must be, because in the class 'Zone', there is no global knowledge yet about surrounding zones
+
+        //    // the original brep needs to split into subsurfaces, according to congruencies with adjacent zones
+        //    // i.e. if it was a box with only 4 wall surfaces, but there is a partly congruent wall that is
+        //    //  e.g. half external and half in contact with another surface, then this 1 surface needs to be split in 2 
+        //    //  and this.Zones[i].Geometry updated
+
+        //}
+    }
 
 
+    /// <summary>
+    /// Building types
+    /// </summary>
+    public enum BuildingType
+    {
+        Residential,
+        Office,
+        Industry,
+        Laboratory,
+        Supermarket,
+        School,
+        Undefined
     }
 }
