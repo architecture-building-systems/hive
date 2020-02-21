943ba5aa961a6d4f1df8df94ed164449fa7d6aae
Christoph Waibel <christophwaibel@gmx.de>
2020-02-21 11:40:28 +0100

    introducing abstract base classes for building components, constructions and materials

--- Mothercell/Hive.IO/Hive.IO/BuildingConstruction.cs
+++ Mothercell/Hive.IO/Hive.IO/BuildingConstruction.cs
@@ -4,26 +4,51 @@ namespace Hive.IO
 {
     namespace BuildingConstruction
     {
-        /// <summary>
-        /// Opaque construction element
-        /// </summary>
-        public struct Opaque
+        public abstract class Construction
         {
             /// <summary>
             /// Name of construction element, e.g. 'concrete30mm'
             /// </summary>
-            public string Name;
+            public string Name { get; private set; }
             /// <summary>
             /// Material name, e.g. 'concrete'
             /// </summary>
-            public string Material;
+            public string Material { get; private set; }
             /// <summary>
             /// [mm]
             /// </summary>
-            public double Thickness;
+            public double Thickness { get; private set; }
+
+
+            #region High level physical properties
+            /// <summary>
+            /// U-Value of this component
+            /// </summary>
+            public double UValue { get; private set; }
+            /// <summary>
+            /// Absorptivity
+            /// </summary>
+            public double Absorbtivity { get; private set; }
+            /// <summary>
+            /// Reflectivity
+            /// </summary>
+            public double Reflectivity { get; private set; }
+            /// <summary>
+            /// Transmissivity
+            /// </summary>
+            public double Transmissivity { get; private set; }
+            #endregion
         }
 
-        public struct Transparent
+        /// <summary>
+        /// Opaque construction element
+        /// </summary>
+        public class Opaque : Construction
+        {
+          
+        }
+
+        public class Transparent : Construction
         {
 
         }
--- Mothercell/Hive.IO/Hive.IO/BuildingMaterial.cs
+++ Mothercell/Hive.IO/Hive.IO/BuildingMaterial.cs
@@ -8,44 +8,49 @@ namespace Hive.IO
 {
     namespace BuildingMaterial
     {
-        public struct Opaque
+        public abstract class Material
         {
             /// <summary>
             /// Name
             /// </summary>
-            public string Name;
+            public string Name { get; private set; }
             /// <summary>
             /// lambda, [W/mK]
             /// </summary>
-            public double Conductivity;
+            public double Conductivity { get; private set; }
             /// <summary>
             /// rho, [kg/m^3]
             /// </summary>
-            public double Density;
+            public double Density { get; private set; }
             /// <summary>
             /// c, [J/kgK]
             /// </summary>
-            public double SpecificHeatCapacity;
+            public double SpecificHeatCapacity { get; private set; }
             /// <summary>
             /// [-]
             /// </summary>
-            public double ThermalAbsorptance;
+            public double ThermalAbsorptance { get; private set; }
             /// <summary>
             /// [-]
             /// </summary>
-            public double SolarAbsorptance;
+            public double SolarAbsorptance { get; private set; }
             /// <summary>
             /// [-]
             /// </summary>
-            public double VisibleAbsorptance;
+            public double VisibleAbsorptance { get; private set; }
         }
 
-        public struct Glass
+        public class Opaque : Material
         {
 
         }
 
-        public struct Gas
+        public class Glass : Material
+        {
+
+        }
+
+        public class Gas : Material
         {
 
         }
