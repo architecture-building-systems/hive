8da8d125fc2e2b9ffb296a71dfdece530ce6fe1f
Christoph Waibel <christophwaibel@gmx.de>
2020-02-25 16:12:32 +0100

    changed EDandDHW component to elec demand only

--- Core/sia380/sia380_4.py
+++ Core/sia380/sia380_4.py
@@ -1,16 +1,11 @@
 """
 Calculating electricity and domestic hot water demand
-according to SIA380-4
+currently just multiplying usage hours times area times specific electricity demand
+
+But in future built, according to SIA380-4?
 """
 
 
-def sia380_4(nfa_total, nfa_laundry, nfa_rooms, ann_el_demand, ann_el_req_light, ann_el_req_simple, washing_equip,
-               washing_light, washing_vent, n_room_equip, light_in_adj_room, n_room_vent, dhw_demand, washing, adj_room):
+def sia380_4(nfa, t_L, t_A, t_V, L, A, V):
 
-    nfa = (nfa_total - nfa_laundry) - nfa_rooms
-    electricity = (ann_el_demand * nfa) + (ann_el_req_light * nfa) + (ann_el_req_simple * nfa) + \
-                  (washing_equip * nfa_laundry) + (washing_light * nfa_laundry) + (washing_vent * nfa_laundry) + \
-                  (n_room_equip * nfa_rooms) + (light_in_adj_room * nfa_rooms) + (n_room_vent * nfa_rooms)
-    # dhw = (dhw_demand * nfa) + (washing * nfa) + (adj_room * nfa)
-
-    return electricity
+    return nfa * t_L * L + nfa * t_A * A + nfa * t_V * V
