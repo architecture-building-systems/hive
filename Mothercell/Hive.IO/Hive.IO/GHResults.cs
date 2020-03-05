d492a7b7be5af5163c940d8e97bf7ee00890879f
Christoph Waibel <christophwaibel@gmx.de>
2020-03-05 18:49:13 +0100

    started results component

---
+++ Mothercell/Hive.IO/Hive.IO/GHResults.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace Hive.IO
+{
+    public class GHResults : GH_Component
+    {
+
+        public GHResults()
+          : base("GHResults", "GHResults",
+              "GHResults",
+              "[hive]", "Mothercell")
+        {
+        }
+
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+        }
+
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+        }
+
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+
+            // also sub-GHResults components, like distributor? Or just one massive Results reader?
+
+            // input: all kinds of results from the Core 
+
+            // output: jsons for Darens Visualizer, and GHREsults object for an updated Visualizer component
+        }
+
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("23cb5778-5a53-4dcc-ba2c-56d51c9c06b3"); }
+        }
+    }
+}
