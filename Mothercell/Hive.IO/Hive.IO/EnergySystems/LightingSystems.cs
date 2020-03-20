e1c30cb7d74b6176b814e2d496962026a84ed71c
Christoph Waibel <christophwaibel@gmx.de>
2020-03-20 18:57:57 +0100

    re-designed the EnergySystems namespace

---
+++ Mothercell/Hive.IO/Hive.IO/EnergySystems/LightingSystems.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hive.IO.EnergySystems
+{
+    /// <summary>
+    /// Electric lighting and daylight
+    /// Including set points, daylight controls, luminaire technology, etc...
+    /// </summary>
+    public abstract class Lighting
+    {
+
+    }
+
+}
