a89c5d90aa5e41dbb6a8b03812b661fac5a8db6e
Christoph Waibel <christophwaibel@gmx.de>
2020-06-23 17:31:32 +0200

    prepared EnergyCarrier class
    each energy technology needs carriers for inputs and outputs

--- Mothercell/Hive.IO/Hive.IO/EnergySystems/Conversion.cs
+++ Mothercell/Hive.IO/Hive.IO/EnergySystems/Conversion.cs
@@ -123,69 +123,91 @@ namespace Hive.IO
         public abstract class Conversion
         {
             /// <summary>
-            /// Name of the technology
+            /// Technology name
             /// </summary>
-            public TechnologyNames Name;
-            public enum TechnologyNames
-            {
-                Boiler,
-                CombinedHeatPower,
-                HeatPump,
-                Photovoltaic,
-                HybridPhotovoltaicThermal,
-                SolarCollector,
-                GroundCollector,
-            }
+            public string Name { get; protected set; }
+            /// <summary>
+            /// Indicating whether this technology produces electricity
+            /// </summary>
+            public bool IsElectric { get; protected set; }
+            /// <summary>
+            /// Indicating whether this technology produces heat
+            /// </summary>
+            public bool IsHeating { get; protected set; }
+            /// <summary>
+            /// Indicating whether this technology produces cooling
+            /// </summary>
+            public bool IsCooling { get; protected set; }
+            /// <summary>
+            /// Capacity of technology. Unit is defined in 'CapacityUnit'
+            /// </summary>
+            public double Capacity { get; protected set; }
+
+            /// <summary>
+            /// Unit of technology capacity (e.g. "kW", or "sqm", etc.)
+            /// </summary>
+            public string CapacityUnit { get; protected set; }
+
+
+
             /// <summary>
             /// Specification of the technology, e.g. "Mono-cristalline PV"
             /// </summary>
             public string DetailedName { get; protected set; }
 
             /// <summary>
-            /// Investment cost per m2
+            /// Investment cost per this.CapacityUnit
             /// </summary>
-            public double InvestmentCost { get; protected set; }
+            public double SpecificInvestmentCost { get; protected set; }
             /// <summary>
-            /// Life cycle GHG emissions, in kgCO2eq./m2
+            /// Life cycle GHG emissions, in kgCO2eq. per this.CapacityUnit
             /// </summary>
-            public double EmbodiedGhg { get; protected set; }
+            public double SpecificEmbodiedGhg { get; protected set; }
 
 
             /// <summary>
             /// Input streams. e.g. for a CHP this could be 'NaturalGas'
             /// </summary>
-            public List<Carrier> InputCarriers { get; protected set; }
+            public List<EnergyCarrier> InputCarriers { get; protected set; }
             /// <summary>
             /// Output streams. e.g. for a CHP this could be 'Water' and 'Electricity'
             /// </summary>
-            public List<Carrier> OutputCarriers { get; protected set; }
-            /// <summary>
-            /// Conversion matrix of size this.InputCarriers x this.OutputCarriers.
-            /// e.g. [input1 input2 input2] x [eta1 eta2; eta3 eta4; eta5 eta6] = [output1 output2]
-            /// </summary>
-            public double [,] ConversionMatrix { get; protected set; }
+            public List<EnergyCarrier> OutputCarriers { get; protected set; }
+
+
 
             /// <summary>
-            /// Capacity of technology. Unit is defined in 'CapacityUnit'
+            /// Time series of thermal conversion efficiency
             /// </summary>
-            public double Capacity { get; private set; }
+            public double [] ConversionEfficiencyHeating { get; protected set; }
             /// <summary>
-            /// Unit of technology capacity (e.g. "kW", or "sqm", etc.)
+            /// Time series of thermal conversion efficiency
             /// </summary>
-            public string CapacityUnit { get; private set; }
-        
+            public double[] ConversionEfficiencyCooling { get; protected set; }
+            /// <summary>
+            /// Time series of thermal conversion efficiency
+            /// </summary>
+            public double [] ConversionEfficiencyElectric { get; protected set; }
+
+
+
 
             // Operation and Maintenance cost? How to deal with conversionMatrix. Is the production always resulting in the same output ratio? I guess, because it is only one conversionMatrix
             // also, how to arrange conversionMatrix, which eff term corresponds to what
             // for now, 
 
 
-            protected Conversion(double investmentCost, double embodiedGhg, List<Carrier> inputCarriers, List<Carrier> outputCarriers, double [,] conversionMatrix)
+
+
+
+
+            protected Conversion(double investmentCost, double embodiedGhg, List<EnergyCarrier> inputCarriers, List<EnergyCarrier> outputCarriers, double?[] conversionEfficiencyHeating)
             {
-                this.InvestmentCost = investmentCost;
-                this.EmbodiedGhg = embodiedGhg;
-                this.InputCarriers = new List<Carrier>(inputCarriers);
-                this.OutputCarriers = new List<Carrier>(outputCarriers);
+                this.SpecificInvestmentCost = investmentCost;
+                this.SpecificEmbodiedGhg = embodiedGhg;
+                this.ConversionEfficiencyHeating = conversionEfficiencyHeating;
+                this.InputCarriers = new List<EnergyCarrier>(inputCarriers);
+                this.OutputCarriers = new List<EnergyCarrier>(outputCarriers);
 
             }
         }
---
+++ Mothercell/Hive.IO/Hive.IO/EnergySystems/EnergyCarrier.cs
@@ -0,0 +1,158 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Hive.IO.EnergySystems
+{
+    #region implemented Energy Carriers
+    /// <summary>
+    /// Air energy carrier, e.g. for air source heat pump
+    /// </summary>
+    public class Air : EnergyCarrier
+    {
+        public Air(int horizon, double[] airTemperature) 
+            : base(horizon, EnergyCarrier.EnergyUnit.DegreeCelsius, airTemperature, null, null) { }
+    }
+
+
+    /// <summary>
+    /// Solar energy carrier, e.g. for PV and solar thermal
+    /// </summary>
+    public class Solar : EnergyCarrier
+    {
+        public Solar(int horizon, double[] irradiation) 
+            : base(horizon, EnergyCarrier.EnergyUnit.KiloWattHoursPerSquareMeters, irradiation, null, null) { }
+    }
+
+
+    /// <summary>
+    /// Water as energy carrier, e.g. the output of a boiler, heat pump, or solar thermal collector
+    /// </summary>
+    public class Water : EnergyCarrier
+    {
+        public Water(int horizon, double[] waterTemperature) 
+            : base(horizon, EnergyCarrier.EnergyUnit.DegreeCelsius, waterTemperature, null, null) { }
+    }
+
+
+    /// <summary>
+    /// Electricity grid provides electricity
+    /// </summary>
+    public class ElectricityGrid : EnergyCarrier
+    {
+        public ElectricityGrid(int horizon, double[] energyCost, double[] ghgEmissions) 
+            : base(horizon, EnergyCarrier.EnergyUnit.KiloWattHours, null, energyCost, ghgEmissions) { }
+    }
+
+
+    /// <summary>
+    /// District Heating provides hot water
+    /// </summary>
+    public class DistrictHeating : EnergyCarrier
+    {
+        public DistrictHeating(int horizon, double[] availableHeating, double[] energyCost, double[] ghgEmissions) 
+            : base(horizon, EnergyCarrier.EnergyUnit.KiloWattHours, availableHeating, energyCost, ghgEmissions) { }
+    }
+
+
+    /// <summary>
+    /// District cooling provides cool water
+    /// </summary>
+    public class DistrictCooling : EnergyCarrier
+    {
+        public DistrictCooling(int horizon, double[] availableCooling, double[] energyCost, double[] ghgEmissions) 
+            : base(horizon, EnergyCarrier.EnergyUnit.KiloWattHours, availableCooling, energyCost, ghgEmissions) { }
+    }
+
+
+    /// <summary>
+    /// BioGas. Availability might be limited
+    /// </summary>
+    public class BioGas : EnergyCarrier
+    {
+        public BioGas(int horizon, double[] biogasAvailability, double[] energyCost, double[] ghgEmissions) 
+            : base(horizon, EnergyCarrier.EnergyUnit.KiloWattHours, biogasAvailability, energyCost, ghgEmissions)
+        {
+        }
+    }
+
+
+    /// <summary>
+    /// Wood pellets. Availability might be limited
+    /// </summary>
+    public class Pellets : EnergyCarrier
+    {
+        public Pellets(int horizon, double[] pelletsAvailability, double[] energyCost, double[] ghgEmissions) 
+            : base(horizon, EnergyCarrier.EnergyUnit.KiloWattHours, pelletsAvailability, energyCost, ghgEmissions)
+        {
+        }
+    }
+#endregion
+
+
+
+    /// <summary>
+    /// Energy Carrier
+    /// </summary>
+    public abstract class EnergyCarrier
+    {
+        /// <summary>
+        /// Pre-defined energy units
+        /// </summary>
+        public enum EnergyUnit
+        {
+            KiloWattHours,
+            KiloWattHoursPerSquareMeters,
+            DegreeCelsius
+        }
+
+        /// <summary>
+        /// Horizon of time series data
+        /// </summary>
+        public int Horizon { get; protected set; }
+        /// <summary>
+        /// Time series of available/provided energy of this carrier in kWh per time step
+        /// </summary>
+        public double [] Energy { get; protected set; }
+
+        /// <summary>
+        /// Setting the energy unit to kWh
+        /// </summary>
+        public EnergyUnit Unit { get; protected set; }
+
+        /// <summary>
+        /// Time series with cost coefficients per kWh
+        /// </summary>
+        public double [] EnergyCost { get; protected set; }
+        /// <summary>
+        /// Time series of Greenhouse gas emissions in kgCO2eq/kWh
+        /// </summary>
+        public double [] GhgEmissions { get; protected set; }
+
+
+        protected EnergyCarrier(int horizon, EnergyUnit unit, double [] energy, double [] energyCost, double [] ghgEmissions)
+        {
+            this.Horizon = horizon;
+            this.Unit = unit;
+
+            // energy could be unbound, e.g. electricity grid, in which case energy == null
+            if (energy != null && energy.Length != 0)
+            {
+                this.Energy = new double[energy.Length];
+                energy.CopyTo(this.Energy, 0);
+            }
+
+            // energy could be free, e.g. air, in which case energyCost == null
+            if (energyCost != null && energyCost.Length != 0)
+            {
+                this.EnergyCost = new double[energyCost.Length];
+                energyCost.CopyTo(this.EnergyCost, 0);
+            }
+
+            // energy could have zero emissions, e.g. air or solar, in which case ghgEmissions == null
+            if (ghgEmissions != null && ghgEmissions.Length != 0)
+            {
+                this.GhgEmissions = new double[ghgEmissions.Length];
+                ghgEmissions.CopyTo(this.GhgEmissions, 0);
+            }
+        }
+    }
+}
