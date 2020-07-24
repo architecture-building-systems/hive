b91731d06f6c03b41c11e1b40c71f088c6b003df
Daren Thomas <daren-thomas@users.noreply.github.com>
2020-07-24 13:02:18 +0200

    format document

--- Mothercell/Hive.IO/Hive.IO/ResultsPlotting.cs
+++ Mothercell/Hive.IO/Hive.IO/ResultsPlotting.cs
@@ -15,8 +15,8 @@
         #region Emissions
         public double EmbodiedEmissionsBuildings(bool normalized) => normalized ? EmbodiedEmissionsBuildings(false) / TotalFloorArea : 1000;
         public double EmbodiedEmissionsSystems(bool normalized) => normalized ? EmbodiedEmissionsSystems(false) / TotalFloorArea : 2000;
-        public double OperationEmissionsBuildings(bool normalized) => normalized? OperationEmissionsBuildings(false) / TotalFloorArea: 3000;
-        public double OperationEmissionsSystems(bool normalized) => normalized? OperationEmissionsSystems(false) / TotalFloorArea: 4000;
+        public double OperationEmissionsBuildings(bool normalized) => normalized ? OperationEmissionsBuildings(false) / TotalFloorArea : 3000;
+        public double OperationEmissionsSystems(bool normalized) => normalized ? OperationEmissionsSystems(false) / TotalFloorArea : 4000;
 
         public double TotalEmbodiedEmissions(bool normalized) =>
             EmbodiedEmissionsBuildings(normalized) + EmbodiedEmissionsSystems(normalized);
@@ -29,10 +29,10 @@
         #endregion Emissions
 
         #region Costs
-        public double EmbodiedCostsBuildings(bool normalized) => normalized? EmbodiedCostsBuildings(false) / TotalFloorArea: 1100;
-        public double EmbodiedCostsSystems(bool normalized) => normalized? EmbodiedCostsSystems(false) / TotalFloorArea: 2200;
-        public double OperationCostsBuildings(bool normalized) => normalized? OperationCostsBuildings(false) / TotalFloorArea: 3300;
-        public double OperationCostsSystems(bool normalized) =>normalized? OperationCostsSystems(false) / TotalFloorArea: 4400;
+        public double EmbodiedCostsBuildings(bool normalized) => normalized ? EmbodiedCostsBuildings(false) / TotalFloorArea : 1100;
+        public double EmbodiedCostsSystems(bool normalized) => normalized ? EmbodiedCostsSystems(false) / TotalFloorArea : 2200;
+        public double OperationCostsBuildings(bool normalized) => normalized ? OperationCostsBuildings(false) / TotalFloorArea : 3300;
+        public double OperationCostsSystems(bool normalized) => normalized ? OperationCostsSystems(false) / TotalFloorArea : 4400;
 
         public double TotalEmbodiedCosts(bool normalized) =>
             EmbodiedCostsBuildings(normalized) + EmbodiedCostsSystems(normalized);
@@ -44,9 +44,9 @@
         #endregion Costs
 
         #region Energy
-        public double EmbodiedEnergyBuildings(bool normalized) =>normalized? EmbodiedEnergyBuildings(false) / TotalFloorArea: 1300;
-        public double EmbodiedEnergySystems(bool normalized) => normalized? EmbodiedEnergySystems(false) / TotalFloorArea: 2300;
-        public double OperationEnergyBuildings(bool normalized) => normalized? OperationEnergyBuildings(false) / TotalFloorArea: 3300;
+        public double EmbodiedEnergyBuildings(bool normalized) => normalized ? EmbodiedEnergyBuildings(false) / TotalFloorArea : 1300;
+        public double EmbodiedEnergySystems(bool normalized) => normalized ? EmbodiedEnergySystems(false) / TotalFloorArea : 2300;
+        public double OperationEnergyBuildings(bool normalized) => normalized ? OperationEnergyBuildings(false) / TotalFloorArea : 3300;
         public double OperationEnergySystems(bool normalized) => normalized ? OperationEnergySystems(false) / TotalFloorArea : 4400;
 
         public double TotalEmbodiedEnergy(bool normalized) =>
