6526158d754f1fca5fc53d07f48ff5d2df656fda
Daren Thomas <thomas@arch.ethz.ch>
2020-08-04 11:07:13 +0200

    updating .gitignore

--- src/Hive.IO/Hive.IO/Plots/EnergyBalancePlot.cs
+++ src/Hive.IO/Hive.IO/Plots/EnergyBalancePlot.cs
@@ -21,11 +21,14 @@ namespace Hive.IO.Plots
         {
             _bounds = bounds;
             var houseBounds = bounds.CloneInflate(-bounds.Width / 3, -bounds.Height / 4);
+            var innerHouseBounds = RenderHouse(graphics, houseBounds);
 
-            RenderHouse(graphics, houseBounds);
+            // inner axis, centered inside the house, left is end point of gains, right is starting point of losses
+            var middleAxis = innerHouseBounds.CloneInflate(-innerHouseBounds.Width / 4f, -10);
+            graphics.DrawRectangleF(new Pen(Color.Aqua), middleAxis);
         }
 
-        private void RenderHouse(Graphics graphics, RectangleF bounds)
+        private RectangleF RenderHouse(Graphics graphics, RectangleF bounds)
         {
             var house = HousePolygon(bounds);
             graphics.FillPolygon(new SolidBrush(Color.LightSlateGray), house);
@@ -34,6 +37,9 @@ namespace Hive.IO.Plots
             var innerHouse = HousePolygon(bounds.CloneInflate(-50, -50));
             graphics.FillPolygon(new SolidBrush(Color.White), innerHouse);
             graphics.DrawPolygon(new Pen(Color.Black), innerHouse);
+
+            return new RectangleF(innerHouse[0].X, innerHouse[1].Y, innerHouse[3].X - innerHouse[0].X,
+                innerHouse[0].Y - innerHouse[1].Y);
         }
 
         private static PointF[] HousePolygon(RectangleF bounds)
@@ -47,11 +53,11 @@ namespace Hive.IO.Plots
             var house = new PointF[]
             {
                 // start at bottom left, clockwise
-                new PointF(xLeft, yBottom),
-                new PointF(xLeft, yRoof),
-                new PointF(xMiddle, yTop),
-                new PointF(xRight, yRoof),
-                new PointF(xRight, yBottom),
+                new PointF(xLeft, yBottom),  // 0
+                new PointF(xLeft, yRoof),  // 1
+                new PointF(xMiddle, yTop),  // 2
+                new PointF(xRight, yRoof),  // 3
+                new PointF(xRight, yBottom),  // 4
             };
             return house;
         }
