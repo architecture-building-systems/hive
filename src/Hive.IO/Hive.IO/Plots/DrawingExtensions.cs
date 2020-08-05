832aa1b5353ca01882cff014bfd44489e1a271a1
Daren Thomas <thomas@arch.ethz.ch>
2020-08-05 10:01:35 +0200

    remove unused using directives in file

--- src/Hive.IO/Hive.IO/Plots/DrawingExtensions.cs
+++ src/Hive.IO/Hive.IO/Plots/DrawingExtensions.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using Grasshopper.Kernel;
-using Grasshopper.Kernel.Types.Transforms;
 
 namespace Hive.IO.Plots
 {
