9d2e0c46336eebda210c77ae4928bcb30ef9a512
Daren Thomas <thomas@arch.ethz.ch>
2020-08-27 10:16:46 +0200

    started documenting hierarchy

---
+++ src/Hive.IO/Hive.IO/Forms/EnergySystemsInput.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Hive.IO.Forms
+{
+    /// <summary>
+    /// Interaction logic for EnergySystemsInput.xaml
+    /// </summary>
+    public partial class EnergySystemsInput
+    {
+        public EnergySystemsInput()
+        {
+            InitializeComponent();
+        }
+    }
+}
