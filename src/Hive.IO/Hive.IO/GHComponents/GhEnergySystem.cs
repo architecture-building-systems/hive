1b7f83447115b9404fe51a53f28a65403e22fef0
Daren Thomas <thomas@arch.ethz.ch>
2020-08-28 11:35:01 +0200

    setting up showing the dialog

--- src/Hive.IO/Hive.IO/GHComponents/GhEnergySystem.cs
+++ src/Hive.IO/Hive.IO/GHComponents/GhEnergySystem.cs
@@ -8,6 +8,7 @@ using Grasshopper.Kernel.Attributes;
 using Grasshopper.Kernel.Types;
 using Rhino.Geometry;
 using Hive.IO.EnergySystems;
+using Hive.IO.Forms;
 using Hive.IO.GhParametricInputs;
 //using Newtonsoft.Json;
 
@@ -15,19 +16,15 @@ namespace Hive.IO.GHComponents
 {
     public class GhEnergySystem : GH_Component
     {
-        public string Form_SystemType { get; set; }
-        public double Form_pv_eff { get; set; }
-        public double Form_thermal_eff { get; set; }
-        public double Form_pv_cost { get; set; }
-        public double Form_pv_co2 { get; set; }
-        public string Form_pv_name { get; set; }
-        private int indexNow { get; set; }
-
 
         public GhEnergySystem()
-          : base("Input EnergySystems Hive", "HiveInputEnergySystems", "Hive.IO.EnergySystems input component (solar energy systems, other conversion technologies, emitters)." +
+            : base("Input EnergySystems Hive", "HiveInputEnergySystems",
+                "Hive.IO.EnergySystems input component (solar energy systems, other conversion technologies, emitters)." +
                 "\nThe component opens a Form upon double click, where details of the solar energy system can be specified." +
-                "\nPossible technologies are Photovoltaic (PV), Solar Thermal (ST), hybrid PVT, or Ground Collector (GC).", "[hive]", "IO") { indexNow = 0; }
+                "\nPossible technologies are Photovoltaic (PV), Solar Thermal (ST), hybrid PVT, or Ground Collector (GC).",
+                "[hive]", "IO")
+        {
+        }
 
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
@@ -45,148 +42,36 @@ namespace Hive.IO.GHComponents
         }
 
 
-        #region WindowsForm
+        #region GhEnergySystemAttributes
         public override void CreateAttributes()
         {
-            m_attributes = new MySpecialComponentAttributes(this);
+            m_attributes = new GhEnergySystemAttributes(this);
         }
-        private class MySpecialComponentAttributes : GH_ComponentAttributes
+        private class GhEnergySystemAttributes : GH_ComponentAttributes
         {
-            public MySpecialComponentAttributes(IGH_Component component)
+            public GhEnergySystemAttributes(IGH_Component component)
             : base(component)
             { }
 
             public override GH_ObjectResponse RespondToMouseDoubleClick(GH_Canvas sender, GH_CanvasMouseEvent e)
             {
-                (Owner as GhEnergySystem)?.DisplayForm();
+                (Owner as GhEnergySystem)?.ShowForm();
                 return GH_ObjectResponse.Handled;
             }
         }
+        #endregion GhEnergySystemAttributes
 
-        FormEnSysSolar _form;
-        public void DisplayForm()
-        {
-            if (_form != null)
-                return;
-
-            _form = new FormEnSysSolar();
-            _form.comboBox1.SelectedIndex = indexNow;
-            _form.textBox1.Text = _form.ElectricEfficiency[indexNow].ToString();// Convert.ToString(Form_pv_eff);
-            _form.textBox2.Text = _form.Cost[indexNow].ToString();
-            _form.textBox3.Text = _form.CO2[indexNow].ToString();
-            _form.textBox4.Text = _form.ThermalEfficiency[indexNow].ToString();
-            _form.pictureBox1.Image = _form.Image[indexNow];
-            _form.helpProvider1.SetHelpString(_form.pictureBox1, _form.HelperText[indexNow]);
-
-            _form.FormClosed += OnFormClosed;
-
-            _form.comboBox1.SelectedIndexChanged += ComboBox1_ItemChanged;
-            _form.textBox1.TextChanged += TextBox_TextChanged;
-            _form.textBox2.TextChanged += TextBox_TextChanged;
-            _form.textBox3.TextChanged += TextBox_TextChanged;
-            _form.textBox4.TextChanged += TextBox_TextChanged;
-
-            _form.radioButton1.CheckedChanged += RadioButton_Changed;
-            _form.radioButton2.CheckedChanged += RadioButton_Changed;
-            _form.radioButton3.CheckedChanged += RadioButton_Changed;
-            _form.radioButton4.CheckedChanged += RadioButton_Changed;
-
-            GH_WindowsFormUtil.CenterFormOnCursor(_form, true);
-            _form.Show(Grasshopper.Instances.DocumentEditor);
-            _form.Location = Cursor.Position;
-        }
-
-        private void Form_Update_Display()
-        {
-            // when radio buttons change, text on the form needs to change
-            indexNow = 0;
-            _form.comboBox1.SelectedIndex = indexNow;
-            _form.textBox1.Text = _form.ElectricEfficiency[indexNow].ToString();// Convert.ToString(Form_pv_eff);
-            _form.textBox2.Text = _form.Cost[indexNow].ToString();
-            _form.textBox3.Text = _form.CO2[indexNow].ToString();
-            _form.textBox4.Text = _form.ThermalEfficiency[indexNow].ToString();
-            _form.pictureBox1.Image = _form.Image[indexNow];
-            _form.helpProvider1.SetHelpString(_form.pictureBox1, _form.HelperText[indexNow]);
-        }
-
-        private void RadioButton_Changed(object sender, EventArgs e)
-        {
-            RadioButton rb = sender as RadioButton;
-            if (rb != null)
-            {
-                if (rb.Checked)
-                {
-                     switch (rb.Name)
-                    {
-                        default:
-                        case "radioButton1":
-                            _form.LoadData("pv");
-                            break;
-                        case "radioButton2":
-                            _form.LoadData("st");
-                            break;
-                        case "radioButton3":
-                            _form.LoadData("pvt");
-                            break;
-                        case "radioButton4":
-                            _form.LoadData("gc");
-                            break;
-                    }
-                }
-            }
-            Form_Update_Display();
-            //Form_Update();
-        }
-
-        private void ComboBox1_ItemChanged(object sender, EventArgs e)
-        {
-            int i = _form.comboBox1.SelectedIndex;
-            
-            _form.pictureBox1.Image = _form.Image[i];
-            _form.textBox1.Text = _form.ElectricEfficiency[i].ToString();
-            _form.textBox2.Text = _form.Cost[i].ToString();
-            _form.textBox3.Text = _form.CO2[i].ToString();
-            _form.textBox4.Text = _form.ThermalEfficiency[i].ToString();
-            _form.helpProvider1.SetHelpString(_form.pictureBox1, _form.HelperText[i]);
-
-            //Form_Update();
-        }
-        
-        private void TextBox_TextChanged(object sender, EventArgs e)
-        {
-            //Form_Update();
-        }
-
-        private void Form_Update()
-        {
-            Form_pv_eff = Convert.ToDouble(_form.textBox1.Text);
-            Form_pv_cost = Convert.ToDouble(_form.textBox2.Text);
-            Form_pv_co2 = Convert.ToDouble(_form.textBox3.Text);
-            Form_thermal_eff = Convert.ToDouble(_form.textBox4.Text);
-            Form_pv_name = _form.comboBox1.SelectedItem.ToString();
-            Form_SystemType = _form.SystemType;
-
-            indexNow = _form.comboBox1.SelectedIndex;
-
-            ExpireSolution(true);
-        }
-
-        private void OnFormClosed(object sender, FormClosedEventArgs formClosedEventArgs)
-        {
-            Form_Update();
-            _form = null;
-        }
 
         protected override void AppendAdditionalComponentMenuItems(ToolStripDropDown menu)
         {
-            Menu_AppendItem(menu, "Show UI", ShowUiClicked, null, true, false);
+            Menu_AppendItem(menu, "Show UI", (sender, Exposure) => ShowForm(), null, true, false);
         }
-        private void ShowUiClicked(object sender, EventArgs e)
-        {
-            DisplayForm();
-        }
-        #endregion 
 
+        private void ShowForm()
+        {
+            var form = new EnergySystemsInput();
+            form.ShowDialog();
+        }
 
         protected override void SolveInstance(IGH_DataAccess DA)
         {
@@ -227,20 +112,20 @@ namespace Hive.IO.GHComponents
             var conversionTech = new List<ConversionTech>();
 
 
-            if (meshList.Count > 0)
-            {
-                foreach (Mesh mesh in meshList)
-                {
-                    if (Form_SystemType == "pv") 
-                        conversionTech.Add(new Photovoltaic(Form_pv_cost, Form_pv_co2, mesh, Form_pv_name, Form_pv_eff));
-                    else if (Form_SystemType == "pvt") 
-                        conversionTech.Add(new PVT(Form_pv_cost, Form_pv_co2, mesh, Form_pv_name, Form_pv_eff, Form_thermal_eff));
-                    else if (Form_SystemType == "st") 
-                        conversionTech.Add(new SolarThermal(Form_pv_cost, Form_pv_co2, mesh, Form_pv_name, Form_thermal_eff));
-                    else 
-                        conversionTech.Add(new GroundCollector(Form_pv_cost, Form_pv_co2, mesh, Form_pv_name)); // Form_thermal_eff, 
-                }
-            }
+            //if (meshList.Count > 0)
+            //{
+            //    foreach (Mesh mesh in meshList)
+            //    {
+            //        if (Form_SystemType == "pv") 
+            //            conversionTech.Add(new Photovoltaic(Form_pv_cost, Form_pv_co2, mesh, Form_pv_name, Form_pv_eff));
+            //        else if (Form_SystemType == "pvt") 
+            //            conversionTech.Add(new PVT(Form_pv_cost, Form_pv_co2, mesh, Form_pv_name, Form_pv_eff, Form_thermal_eff));
+            //        else if (Form_SystemType == "st") 
+            //            conversionTech.Add(new SolarThermal(Form_pv_cost, Form_pv_co2, mesh, Form_pv_name, Form_thermal_eff));
+            //        else 
+            //            conversionTech.Add(new GroundCollector(Form_pv_cost, Form_pv_co2, mesh, Form_pv_name)); // Form_thermal_eff, 
+            //    }
+            //}
             if (solarTechProperties.Count > 0)
             {
                 foreach (var solarProperties in solarTechProperties)
@@ -307,18 +192,9 @@ namespace Hive.IO.GHComponents
         }
 
 
-        protected override System.Drawing.Bitmap Icon
-        {
-            get
-            {
-                return Hive.IO.Properties.Resources.IO_Solartech;
-            }
-        }
+        protected override System.Drawing.Bitmap Icon => Properties.Resources.IO_Solartech;
 
 
-        public override Guid ComponentGuid
-        {
-            get { return new Guid("59389231-9a1b-4732-99dd-2bdda6b78bb8"); }
-        }
+        public override Guid ComponentGuid => new Guid("59389231-9a1b-4732-99dd-2bdda6b78bb8");
     }
 }
