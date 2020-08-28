facafac3d5d0dec3c0a12fb94616290042249141
Daren Thomas <thomas@arch.ethz.ch>
2020-08-28 09:37:25 +0200

    starting on the viewmodels

---
+++ src/Hive.IO/Hive.IO/Forms/EnergySystemsInputViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Hive.IO.Forms
+{
+    /// <summary>
+    /// A ViewModel to bind the EnergySystemsInput form to. This includes any defaults used and relationships to user controls (ConversionProperties).
+    /// </summary>
+    public class EnergySystemsInputViewModel
+    {
+
+    }
+}
---
+++ src/Hive.IO/Hive.IO/Forms/ViewModels.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace Hive.IO.Forms
+{
+    /// <summary>
+    /// I'm using this link: https://www.codeproject.com/Questions/1255014/Wpf-MVVM-dynamic-binding-of-usercontrols-and-show
+    /// as a basis for setting up the MVVM design pattern. 
+    /// </summary>
+    public abstract class ObservableBase : INotifyPropertyChanged
+    {
+        public void Set<TValue>(ref TValue field,
+            TValue newValue,
+            [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<TValue>.Default.Equals(field, default(TValue))
+                || !field.Equals(newValue))
+            {
+                field = newValue;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public abstract class ViewModelBase : ObservableBase
+    {
+        public bool IsInDesignMode()
+        {
+            return Application.Current.MainWindow == null || DesignerProperties.GetIsInDesignMode(Application.Current.MainWindow);
+        }
+    }
+}
