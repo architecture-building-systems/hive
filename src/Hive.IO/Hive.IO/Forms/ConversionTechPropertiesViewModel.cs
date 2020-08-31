dc2d429a472bdcae1136b790b69381f35aab5422
Daren Thomas <thomas@arch.ethz.ch>
2020-08-31 08:44:10 +0200

    implementing mvvm

--- src/Hive.IO/Hive.IO/Forms/ConversionTechPropertiesViewModel.cs
+++ src/Hive.IO/Hive.IO/Forms/ConversionTechPropertiesViewModel.cs
@@ -2,15 +2,22 @@
 {
     public interface IConversionTechProperties
     {
-
+        string Name { get; }
+        string Source { get; }
+        string EndUse { get; }
     }
 
     public class PhotovoltaicPropertiesViewModel: ViewModelBase, IConversionTechProperties
     {
+        public string Name => "Photovoltaic (PV)";
+        public string Source => "Solar";
+        public string EndUse => "Electricity demand";
     }
 
     public class GasBoilerPropertiesViewModel : ViewModelBase, IConversionTechProperties
     {
-
+        public string Name => "Boiler (Gas)";
+        public string Source => "Gas";
+        public string EndUse => "Heating demand, DHW";
     }
 }
--- src/Hive.IO/Hive.IO/Forms/EnergySystemsInputViewModel.cs
+++ src/Hive.IO/Hive.IO/Forms/EnergySystemsInputViewModel.cs
@@ -1,4 +1,9 @@
-﻿namespace Hive.IO.Forms
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Hive.IO.EnergySystems;
+
+namespace Hive.IO.Forms
 {
     /// <summary>
     /// A ViewModel to bind the EnergySystemsInput form to. This includes any defaults used and relationships to user controls (ConversionProperties).
@@ -11,6 +16,22 @@
         {
             get => _currentConversionTechProperties;
             private set => Set(ref _currentConversionTechProperties, value);
+        }
+
+        private Dictionary<string, Func<IConversionTechProperties>> _conversions =
+            new Dictionary<string, Func<IConversionTechProperties>>
+            {
+                {"Photovoltaic (PV)", () => new PhotovoltaicPropertiesViewModel()},
+                {"Boiler (Gas)", () => new GasBoilerPropertiesViewModel()}
+            };
+
+        public IEnumerable<string> Conversions => _conversions.Keys;
+
+        public ObservableCollection<IConversionTechProperties> ConversionTechnologies { get; private set; }
+
+        public EnergySystemsInputViewModel()
+        {
+            ConversionTechnologies = new ObservableCollection<IConversionTechProperties>();
         }
     }
 }
