938b3769883f6b9473a65a690631dd0eb3b48061
Christoph Waibel <christophwaibel@gmx.de>
2021-01-05 00:07:10 +0100

    updated Hive.IO icons
    Bug: icons are not 24x24 pixel anymore

--- src/Hive.IO/Hive.IO/Properties/Resources.Designer.cs
+++ src/Hive.IO/Hive.IO/Properties/Resources.Designer.cs
@@ -195,16 +195,6 @@ namespace Hive.IO.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap IO_Solartech {
-            get {
-                object obj = ResourceManager.GetObject("IO_Solartech", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap IO_Visualizer {
             get {
                 object obj = ResourceManager.GetObject("IO_Visualizer", resourceCulture);
