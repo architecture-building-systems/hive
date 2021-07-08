f6814c28551dbfb74e8c4ed9ba844f590434c314
maxenceryan <53180223+maxenceryan@users.noreply.github.com>
2021-07-08 12:28:59 +0200

    Add some kind of test framework but very WIP. Test data for sia380 useful for testing purposes

---
+++ tests/Hive.Core/sia380/sia380_test.py
@@ -0,0 +1,26 @@
+import pytest
+import json
+
+import sia380
+
+def test_main(**args):
+    
+    ## NOT WORKING YET
+    
+    # Arrange
+    with open("testdata.json", 'r') as f:
+        kwargs = json.loads(f.read(), encoding='utf-8')
+    
+    import sia380.datatree as dt
+    kwargs['srf_irrad_obstr_tree'] = dt.DataTree([[0.0]*8760])
+    kwargs['srf_irrad_unobstr_tree'] = dt.DataTree(kwargs['srf_irrad_unobstr_tree'])
+    
+    # Act
+    results = sia380.main(**kwargs)
+    
+    # Assert
+    Q_Heat, Q_Cool, Q_Elec, Q_T, Q_V, Q_i, Q_s, Q_T_op, Q_T_tr, Q_s_tr_tree = results
+    results_no_solar = [Q_Heat, Q_Cool, Q_Elec, Q_T, Q_V, Q_i, Q_T_op, Q_T_tr]
+    
+    # TODO
+    assert True
