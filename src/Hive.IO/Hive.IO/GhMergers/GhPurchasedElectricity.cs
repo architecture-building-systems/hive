bf1a39b63887715c3a2f5f4beed6eb33e0f01e08
christophwaibel <christophwaibel@gmx.de>
2022-05-06 16:11:43 +0200

    added final heating and electricity demand to component. lets clean it up later once we actuially make a proper Energy Hub component

--- src/Hive.IO/Hive.IO/GhMergers/GhPurchasedElectricity.cs
+++ src/Hive.IO/Hive.IO/GhMergers/GhPurchasedElectricity.cs
@@ -14,8 +14,9 @@ namespace Hive.IO.GhMergers
         /// Initializes a new instance of the GhGridSubstation class.
         /// </summary>
         public GhPurchasedElectricity()
-          : base("Merger Direct Electricity Hive", "HiveMergerDirectElectricity",
-              "Hive Merger for Direct Electricity, e.g. an electrical substation, or the connection from the grid to the building (<Hive.IO.EnergySystems.DirectElectricity>). It merges all kinds of data together to update the electricity grid connection with information on consumed energy, cost, operational schedule, etc. that have been computed in other components.",
+          : base("Merger Additional Electricity Hive", "HiveMergerAddedElectricity",
+              "Hive Merger for Additional electricity, from conversion technologies such as chillers or ASHP. Also for PV (reduces elec consumption) \n" +
+                "It merges all kinds of data together to update the electricity grid connection with information on consumed energy, cost, operational schedule, etc. that have been computed in other components.",
               "[hive]", "IO-Core")
         {
         }
@@ -27,26 +28,26 @@ namespace Hive.IO.GhMergers
         /// </summary>
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
+            //0
             pManager.AddGenericParameter("Electricity Carrier", "Electricity", "Electricity energy carrier, e.g. from an electricity grid.", GH_ParamAccess.item);
-
+            //1
             pManager.AddGenericParameter("Direct Electricity", "DirectElectricity", "Direct Electricity of type <Hive.IO.EnergySystems.DirectElectricity>, e.g. an electrical substation, or the connection between the electricity grid and the building.", GH_ParamAccess.item);
-
+            //2
             pManager.AddBooleanParameter("hourly", "hourly", "toggle switch for true/false", GH_ParamAccess.item);
-
+            //3
             pManager.AddGenericParameter("Hive Chiller", "Chiller", "Hive Chiller (<Hive.IO.EnergySystems.Chiller>) that will be infused with information from above inputs.", GH_ParamAccess.item);
-
+            //4
             pManager.AddGenericParameter("Hive Air Source Heat Pump", "ASHP", "Hive Air Source Heat Pump (ASHP) (<Hive.IO.EnergySystems.AirSourceHeatPump>) that will be infused with information from above inputs.", GH_ParamAccess.item);
-
-            pManager.AddNumberParameter("Electricity demand", "siaElec", "Electricity loads of a zone in kWh.", GH_ParamAccess.list);
-
+            //5
+            pManager.AddNumberParameter("Ideal Loads Electricity", "IdealElectricity", "Ideal electric loads from appliances and lighting. In kWh, either monthly, or hourly", GH_ParamAccess.list);
+            //6
             pManager.AddGenericParameter("Solar Technologies", "pvOrSTlist", "Hive.IO.EnergySystems.SurfaceBasedTech, such as PV, PVT, Solar Thermal, Ground Collector", GH_ParamAccess.list);
-
-            pManager.AddNumberParameter("Heating demand", "heatDemandIn", "Heating loads of a zone in kWh.", GH_ParamAccess.list);
-
+            //7
+            pManager.AddNumberParameter("Ideal Loads Heating", "IdealHeating", "Ideal heating loads of a zone. In kWh, either monthly, or hourly.", GH_ParamAccess.list);
         }
 
 
-    
+
 
         /// <summary>
         /// Registers all the output parameters for this component.
@@ -55,9 +56,9 @@ namespace Hive.IO.GhMergers
         {
             pManager.AddGenericParameter("Direct Electricity", "DirectElectricity", "DirectElectricity, 'fake' conversion tech (the connection from the building to the electricity grid) 'infused' with elecConsumed from the grid (input carrier) and provided into the building (output carrier), and operational cost and emissions (input carrier)", GH_ParamAccess.item);
 
-            //pManager.AddGenericParameter("Additional Electricity", "Additional Electricity", "Additional Electricity", GH_ParamAccess.item);
+            pManager.AddGenericParameter("Final Heating Demand", "FinalHeating", "Final heating demand, e.g., substracting generated solar thermal from loads", GH_ParamAccess.list);
 
-            //pManager.AddGenericParameter("Electricity Demand Out", "Electricity Demand Out", "Electricity Demand Out",GH_ParamAccess.item);
+            pManager.AddGenericParameter("Final Electricity Demand", "FinalElectricity", "Final electricity demand, i.e., including chiller/ashp/PV", GH_ParamAccess.list);
         }
 
         /// <summary>
@@ -81,80 +82,60 @@ namespace Hive.IO.GhMergers
             AirSourceHeatPump ashp = null;
             DA.GetData(4, ref ashp);
 
-            var siaElec = new List<double>();
-            DA.GetDataList(5, siaElec);
+            var idealElec = new List<double>();
+            DA.GetDataList(5, idealElec);
 
             var solarTech = new List<SurfaceBasedTech>();
             DA.GetDataList(6, solarTech);
 
-            var heatDemandIn = new List<double>();
-            DA.GetDataList(7, heatDemandIn);
+            var idealHeating = new List<double>();
+            DA.GetDataList(7, idealHeating);
 
-            IEnumerable<double> additionalelec = null;
-            if (hourly == false)
-            {
-                if (chiller != null && ashp != null)
-                    additionalelec = chiller.InputCarrier.EnergyMonthlyCumulative.Zip(ashp.InputCarrier.EnergyMonthlyCumulative, (x, y) => x + y).Zip(siaElec, (x, y) => x + y);
-                else if (ashp != null && chiller == null)
-                    additionalelec = ashp.InputCarrier.EnergyMonthlyCumulative.Zip(siaElec, (x, y) => x + y);
-                else if (ashp == null && chiller != null)
-                    additionalelec = chiller.InputCarrier.EnergyMonthlyCumulative.Zip(siaElec, (x, y) => x + y);
-                else
-                    additionalelec = siaElec;
-            }
-            else
-            {
-                if (chiller != null && ashp != null)
-                    additionalelec = chiller.InputCarrier.Energy.Zip(ashp.InputCarrier.Energy, (x, y) => x + y).Zip(siaElec, (x, y) => x + y);
-                else if (ashp != null && chiller == null)
-                    additionalelec = ashp.InputCarrier.Energy.Zip(siaElec, (x, y) => x + y);
-                else if (ashp == null && chiller != null)
-                    additionalelec = chiller.InputCarrier.Energy.Zip(siaElec, (x, y) => x + y);
-                else
-                    additionalelec = siaElec;
-            }
+            int horizon = hourly ? Misc.HoursPerYear : Misc.MonthsPerYear;
+            double[] finalElec = idealElec.ToArray(); //this requires these loads time series to have the correct horizon
+            double[] finalHeat = idealHeating.ToArray();
 
-            var horizon = 8760;
-            if (hourly == false)
-                horizon = 12;
-
-
-            IEnumerable<double> totalRenewableHeat = new double[horizon];
-            IEnumerable<double> totalRenewableElec = new double[horizon];
-            IEnumerable<double> energyRenewable = null;
-            IEnumerable<double> elecPurchased = null;
-
-
-            if (hourly == false)
-            {
-                foreach (var pvOrSt in solarTech)
+            if (!hourly) 
+            { 
+                if (chiller != null)
+                    finalElec = finalElec.Zip(chiller.InputCarrier.EnergyMonthlyCumulative, (x, y) => x + y).ToArray();
+                if (ashp != null)
+                    finalElec = finalElec.Zip(ashp.InputCarrier.EnergyMonthlyCumulative, (x, y) => x + y).ToArray();
+                if(solarTech.Count > 0)
                 {
-                    if (pvOrSt != null)
-                        energyRenewable = pvOrSt.OutputCarriers[0].EnergyMonthlyCumulative;
-                    if (pvOrSt.GetType() == typeof(Photovoltaic) || pvOrSt.GetType() == typeof(BuildingIntegratedPV))
-                        totalRenewableElec = totalRenewableElec.Zip(energyRenewable, (x, y) => x + y);
-                    else if (pvOrSt.GetType() == typeof(SolarThermal))
-                        totalRenewableHeat = totalRenewableHeat.Zip(energyRenewable, (x, y) => x + y);
+                    foreach (var solTech in solarTech)
+                    {
+                        if (solTech.GetType() == typeof(Photovoltaic) || solTech.GetType() == typeof(BuildingIntegratedPV))
+                            finalElec = finalElec.Zip(solTech.OutputCarriers[0].EnergyMonthlyCumulative, (x, y) => x - y).ToArray();
+                        else if (solTech.GetType() == typeof(SolarThermal))
+                            finalHeat = finalHeat.Zip(solTech.OutputCarriers[0].EnergyMonthlyCumulative, (x, y) => x - y).ToArray();
+                    }
                 }
             }
             else
             {
-                foreach (var pvOrSt in solarTech)
+                if (chiller != null)
+                    finalElec = finalElec.Zip(chiller.InputCarrier.Energy, (x, y) => x + y).ToArray();
+                if (ashp != null)
+                    finalElec = finalElec.Zip(ashp.InputCarrier.Energy, (x, y) => x + y).ToArray();
+                if (solarTech.Count > 0)
                 {
-                    if (pvOrSt != null)
-                        energyRenewable = pvOrSt.OutputCarriers[0].Energy;
-                    if (pvOrSt.GetType() == typeof(Photovoltaic) || pvOrSt.GetType() == typeof(BuildingIntegratedPV))
-                        totalRenewableElec = totalRenewableElec.Zip(energyRenewable, (x, y) => x + y);
-                    else if (pvOrSt.GetType() == typeof(SolarThermal))
-                        totalRenewableHeat = totalRenewableHeat.Zip(energyRenewable, (x, y) => x + y);
+                    foreach (var solTech in solarTech)
+                    {
+                        if (solTech.GetType() == typeof(Photovoltaic) || solTech.GetType() == typeof(BuildingIntegratedPV))
+                            finalElec = finalElec.Zip(solTech.OutputCarriers[0].Energy, (x, y) => x - y).ToArray();
+                        else if (solTech.GetType() == typeof(SolarThermal))
+                            finalHeat = finalHeat.Zip(solTech.OutputCarriers[0].Energy, (x, y) => x - y).ToArray();                  
+                    }
                 }
             }
 
-            elecPurchased = additionalelec.Zip(totalRenewableElec, (x, y) => x - y);
 
-            substation.SetInputOutput(elec, elecPurchased.ToArray());
-
+            
+            substation.SetInputOutput(elec, finalElec);
             DA.SetData(0, substation);
+            DA.SetDataList(1, finalHeat.ToList());
+            DA.SetDataList(2, finalElec.ToList());
 
         }
 
